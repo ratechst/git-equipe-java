331656b3d2464399ddb4737cc5ec02b6f430f278
Ratech <145386981+ratechst@users.noreply.github.com>
2025-09-17 16:23:06 +0000

    Codigo base

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # git-equipe-java
+Linha da equipe (edite ESTA MESMA LINHA para criar conflito): Equipe = [PREENCHER AQUI]
---
+++ src/main/java/equipe/Calc.java
@@ -0,0 +1,21 @@
+package com.exemplo.equipe;
+
+public class Calc {
+    // Pessoa A
+    public int soma(int a, int b) {
+        // TODO: implementar
+        return 0; 
+    }
+
+    // Pessoa B
+    public int subtracao(int a, int b) {
+        // TODO: implementar
+        return 0; 
+    }
+
+    // Pessoa C
+    public int multiplicacao(int a, int b) {
+        // TODO: implementar
+        return 0; 
+    }
+}
